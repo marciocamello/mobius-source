48a7f780940ae0232ae0b1f8623d5c9168a3109b
mobius@cyber-wizard.com <mobius@cyber-wizard.com@0fc8a6e8-fb23-9f81-2774-a96632982ed2>
2013-02-12 10:24:30 +0000

    SQL part of the Full Museum Statistics. Contributed by vegax.

---
+++ trunk/dist/tools/sql/server/world_statistic_result_general.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `world_statistic_result_general`;
+
+CREATE TABLE `world_statistic_result_general` (
+`categoryId` int(10) unsigned NOT NULL DEFAULT '0',
+`subCategoryId` int(10) unsigned NOT NULL DEFAULT '0',
+`place` int(10) unsigned NOT NULL DEFAULT '0',
+`charId` int(10) unsigned NOT NULL DEFAULT '0',
+`char_name` varchar(35) NOT NULL,
+`statValue` bigint(20) unsigned NOT NULL DEFAULT '0',
+PRIMARY KEY (`categoryId`,`subCategoryId`,`charId`),
+KEY `Categories` (`categoryId`,`subCategoryId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
