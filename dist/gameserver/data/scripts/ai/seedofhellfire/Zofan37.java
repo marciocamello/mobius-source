c77f1bc52059fe36b5f1c78ab511b3d678e7618e
andreagighi <andreagighi@gmail.com>
2013-02-28 16:16:07 +0000

    Fixed Zofan37 script ai.

--- dist/gameserver/data/scripts/ai/seedofhellfire/Zofan37.java
+++ dist/gameserver/data/scripts/ai/seedofhellfire/Zofan37.java
@@ -1,58 +1,46 @@
 package ai.seedofhellfire;
 
-import java.util.List;
-
 import lineage2.gameserver.ai.CtrlEvent;
 import lineage2.gameserver.ai.Fighter;
 import lineage2.gameserver.model.Creature;
 import lineage2.gameserver.model.instances.NpcInstance;
+import java.util.List;
 
 public class Zofan37 extends Fighter
 {
-	public Zofan37(NpcInstance actor)
-	{
-		super(actor);
-	}
-	
-	@Override
-	protected void onEvtAttacked(Creature attacker, int damage)
-	{
-		if ((attacker == null) || (attacker.isPlayable()))
-		{
-			return;
-		}
-		super.onEvtAttacked(attacker, damage);
-	}
-	
-	@Override
-	public boolean checkAggression(Creature target)
-	{
-		if (target.isPlayable())
-		{
-			return false;
-		}
-		return super.checkAggression(target);
-	}
-	
-	@Override
-	protected boolean thinkActive()
-	{
-		NpcInstance actor = getActor();
-		if (actor.isDead())
-		{
-			return false;
-		}
-		List<NpcInstance> around = actor.getAroundNpc(800, 500);
-		if ((around != null) && (!around.isEmpty()))
-		{
-			for (NpcInstance npc : around)
-			{
-				if (npc.getNpcId() == 51038)
-				{
-					actor.getAI().notifyEvent(CtrlEvent.EVT_ATTACKED, npc, Integer.valueOf(500));
-				}
-			}
-		}
-		return true;
-	}
+  public Zofan37(NpcInstance actor)
+  {
+    super(actor);
+  }
+
+  @Override
+protected void onEvtAttacked(Creature attacker, int damage)
+  {
+    if ((attacker == null) || (attacker.isPlayable()))
+      return;
+    super.onEvtAttacked(attacker, damage);
+  }
+
+  @Override
+public boolean checkAggression(Creature target)
+  {
+    if (target.isPlayable())
+      return false;
+    return super.checkAggression(target);
+  }
+
+  @Override
+protected boolean thinkActive()
+  {
+    NpcInstance actor = getActor();
+    if (actor.isDead()) {
+      return false;
+    }
+    List<NpcInstance> around = actor.getAroundNpc(800, 500);
+    if ((around != null) && (!around.isEmpty()))
+      for (NpcInstance npc : around)
+        if (npc.getNpcId() == 51038)
+          actor.getAI().notifyEvent(CtrlEvent.EVT_ATTACKED, npc, Integer.valueOf(500));
+    return true;
+  }
 }
