abc8e3d95e04c51345e97729c1c3cc212476ee72
mobius@cyber-wizard.com <mobius@cyber-wizard.com@0fc8a6e8-fb23-9f81-2774-a96632982ed2>
2014-08-24 02:01:14 +0000

    Removed double spawns.

--- trunk/java/lineage2/gameserver/GameServer.java
+++ trunk/java/lineage2/gameserver/GameServer.java
@@ -292,6 +292,13 @@ public class GameServer
 			Class.forName(GarbageCollector.class.getName());
 		}
 		
+		// Uncomment to check for double spawns
+		/*
+		 * for (NpcInstance npcInst : GameObjectsStorage.getAllNpcsForIterate()) { final List<NpcInstance> around = npcInst.getAroundNpc(10, 10); if ((around != null) && !around.isEmpty()) { for (NpcInstance npc : around) { if ((npcInst.getNpcId() == npc.getNpcId()) && !npcInst.isMonster() &&
+		 * !npc.getTitle().equals("Double Spawn") && !npcInst.getName().contains("Star Stone") && !npcInst.getName().contains("Wisp")) { npcInst.setTitle("Double Spawn"); npc.setTitle("Double Spawn"); _log.info("Probable double spawn: NpcId " + npc.getNpcId() + " Location " + npc.getSpawnedLoc().x +
+		 * " " + npc.getSpawnedLoc().y + " " + npc.getSpawnedLoc().z + " " + npc.getSpawnedLoc().h); } } } }
+		 */
+		
 		Shutdown.getInstance().schedule(Config.RESTART_AT_TIME, Shutdown.RESTART);
 		_log.info("GameServer Started");
 		_log.info("Maximum Numbers of Connected Players: " + Config.MAXIMUM_ONLINE_USERS);
