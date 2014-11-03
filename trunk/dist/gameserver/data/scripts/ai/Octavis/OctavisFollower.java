1d11b9fbd82338391f7782c0d6134e44336a91fa
mobius@cyber-wizard.com <mobius@cyber-wizard.com@0fc8a6e8-fb23-9f81-2774-a96632982ed2>
2014-11-03 11:39:33 +0000

    Octavis Instance rework. Contributed by Nache.

--- trunk/dist/gameserver/data/scripts/ai/Octavis/OctavisFollower.java
+++ trunk/dist/gameserver/data/scripts/ai/Octavis/OctavisFollower.java
@@ -22,10 +22,21 @@ import lineage2.gameserver.model.World;
 import lineage2.gameserver.model.instances.NpcInstance;
 import lineage2.gameserver.tables.SkillTable;
 
+/**
+ * @author Mobius and Nache
+ */
+
 public final class OctavisFollower extends DefaultAI
 {
-	private static final int Rider = 29192;
+	private static final int OCTAVIS_LIGHT_FIRST = 29191;
+	private static final int OCTAVIS_LIGHT_BEAST = 29192;
+	private static final int OCTAVIS_HARD_FIRST = 29209;
+	private static final int OCTAVIS_HARD_BEAST = 29210;
+	
+	private static final Skill OCTAVIS_RAIN_OF_ARROWS = SkillTable.getInstance().getInfo(14285, 1);
+	
 	private static final int DRIFT_DISTANCE = 10;
+	
 	private long _wait_timeout = 0;
 	private HardReference<? extends Creature> _octavisRef = HardReferences.emptyRef();
 	
@@ -57,16 +68,14 @@ public final class OctavisFollower extends DefaultAI
 	{
 		NpcInstance actor = getActor();
 		Creature octavis = _octavisRef.get();
-		
 		if (octavis == null)
 		{
 			if (System.currentTimeMillis() > _wait_timeout)
 			{
 				_wait_timeout = System.currentTimeMillis() + 1000;
-				
 				for (NpcInstance npc : World.getAroundNpc(actor))
 				{
-					if (npc.getId() == Rider)
+					if ((npc.getId() == OCTAVIS_LIGHT_BEAST) || (npc.getId() == OCTAVIS_HARD_BEAST))
 					{
 						_octavisRef = npc.getRef();
 						return true;
@@ -84,29 +93,55 @@ public final class OctavisFollower extends DefaultAI
 			startSkillAttackTask();
 			return true;
 		}
-		
 		return false;
-	}
-	
-	private void startSkillAttackTask()
-	{
-		NpcInstance npc = getActor();
-		Skill skill = SkillTable.getInstance().getInfo(14285, 1); // TODO[K] - Skills lighting Octavis
-		
-		for (Creature creature : npc.getAroundCharacters(10000, 100))
-		{
-			if (Rnd.chance(30))
-			{
-				npc.altOnMagicUseTimer(creature, skill);
-			}
-		}
-		
-		doTask();
 	}
 	
 	@Override
 	protected void onEvtAttacked(Creature attacker, int damage)
 	{
+		NpcInstance npc = getActor();
+		if ((npc.getId() == OCTAVIS_LIGHT_FIRST) || (npc.getId() == OCTAVIS_HARD_FIRST))
+		{
+			double hp = npc.getCurrentHp() / npc.getMaxHp();
+			if ((hp <= 0.5D) && (npc.getNpcState() == 0))
+			{
+				int effect = (int) (hp * 10.0D);
+				if ((effect < 5) && (effect > 0))
+				{
+					npc.setNpcState(effect);
+				}
+				else if (effect == 0)
+				{
+					npc.setNpcState(5);
+				}
+			}
+			else if ((npc.getCurrentHp() / npc.getMaxHp()) <= 0.01D)
+			{
+				npc.setNpcState(6);
+				npc.setNpcState(0);
+			}
+			else if (((npc.getCurrentHp() / npc.getMaxHp()) > 0.5D) && (npc.getNpcState() == 1))
+			{
+				npc.setNpcState(6);
+				npc.setNpcState(0);
+			}
+		}
+		doTask();
+	}
+	
+	private void startSkillAttackTask()
+	{
+		NpcInstance npc = getActor();
+		
+		Skill skill = OCTAVIS_RAIN_OF_ARROWS; // TODO[K] - Skills lighting Octavis
+		for (Creature creature : npc.getAroundCharacters(10000, 100))
+		{
+			if (Rnd.chance(1))
+			{
+				npc.altOnMagicUseTimer(creature, skill);
+			}
+		}
+		doTask();
 	}
 	
 	@Override
