8f33107985daef616780c20e813ccde1f0a45e4d
mobius@cyber-wizard.com <mobius@cyber-wizard.com@0fc8a6e8-fb23-9f81-2774-a96632982ed2>
2014-11-29 11:57:31 +0000

    [Ertheia] Fixed viewing other player items.

--- beta/java/lineage2/gameserver/network/serverpackets/CharInfo.java
+++ beta/java/lineage2/gameserver/network/serverpackets/CharInfo.java
@@ -204,7 +204,8 @@ public class CharInfo extends L2GameServerPacket
 			writeC(0);
 			writeC(0);
 			writeH(0);
-			writeS(_activeChar.getTitle());
+			// writeS(_activeChar.getTitle());
+			writeS("");
 			if ((template.getLHandId() > 0) || (template.getRHandId() > 0) || (template.getChestId() > 0))
 			{
 				writeH(68);
@@ -266,7 +267,7 @@ public class CharInfo extends L2GameServerPacket
 			
 			for (int slot : PAPERDOLL_ORDER)
 			{
-				writeD(_activeChar.getInventory().getVisualItemId(slot));
+				writeD(_activeChar.getInventory().getPaperdollItemId(slot));
 			}
 			
 			writeH(_activeChar.getInventory().getPaperdollAugmentationId(Inventory.PAPERDOLL_RHAND));
@@ -279,15 +280,20 @@ public class CharInfo extends L2GameServerPacket
 			
 			writeC(_activeChar.getTalismanCount());
 			
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_RHAND));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_LHAND));
-			writeD(0x00);
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_GLOVES));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_CHEST));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_LEGS));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_FEET));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_HAIR));
-			writeD(_activeChar.getInventory().getVisualItemId(Inventory.PAPERDOLL_DHAIR));
+			// for (int slot : PAPERDOLL_ORDER)
+			// {
+			// writeD(_activeChar.getInventory().getPaperdollItemId(slot));
+			// }
+			
+			writeD(0); // Visible Weapon
+			writeD(0); // Visible Shield [Sigil]
+			writeD(0); // Visible Weapon / Two Handed
+			writeD(0); // Visible Gloves
+			writeD(0); // Visible Upper Body
+			writeD(0); // Visible Lower Body
+			writeD(0); // Visible Boots
+			writeD(0); // Visible Hair Accessory (top)
+			writeD(0); // Visible Hair Accessory (bottom)
 			
 			writeC(_activeChar.getPvpFlag());
 			int Karma = 0 - _activeChar.getKarma();
